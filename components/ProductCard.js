fb614bdfd0a8d82003ff2c3ed2d15f7838c385e9
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-01-31 02:00:27 +0000

    Product Cards

---
+++ components/ProductCard.js
@@ -0,0 +1,5 @@
+import styles from '../styles/ProductCard.module.css';
+
+export default function ProductCard() {
+  return <div className={styles.container}></div>;
+}
--- components/ProductsGrid.js
+++ components/ProductsGrid.js
@@ -1,5 +1,29 @@
+import ProductCard from './ProductCard';
 import styles from '../styles/ProductsGrid.module.css';
 
 export default function ProductsGrid() {
-  return <div className={styles.container}>ProductsGrid</div>;
+  return (
+    <div className={styles.container}>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+      <ProductCard></ProductCard>
+    </div>
+  );
 }
