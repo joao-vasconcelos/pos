351678ff4d8e222e84179404e35f598426d9a688
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-01-31 02:18:36 +0000

    Items

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,5 +1,10 @@
 import styles from '../styles/ProductCard.module.css';
 
 export default function ProductCard() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.image}></div>
+      <div className={styles.label}>Product Name</div>
+    </div>
+  );
 }
