1c0903549c22d1bcbe45ca3450e72c8c271fe467
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-02-23 21:32:05 +0000

    Create database.js
    Setup mongodb connection

---
+++ services/database.js
@@ -0,0 +1,31 @@
+/* * * * * */
+/* DATABASE */
+/* * */
+
+/* * */
+/* IMPORTS */
+import config from 'config';
+import mongoose from 'mongoose';
+
+module.exports.connect = async function () {
+  await mongoose
+    .connect(config.get('secrets.database-connection-string'))
+    // .then(() => console.log('Connected.'))
+    .catch((error) => {
+      console.log('Connection to MongoDB failed.');
+      console.log('At database.js > mongoose.connect()');
+      console.log(error);
+      process.exit();
+    });
+};
+
+module.exports.disconnect = async function () {
+  await mongoose
+    .disconnect()
+    // .then(() => console.log('Disconnected from MongoDB.'))
+    .catch((error) => {
+      console.log('Failed closing connection to MongoDB.');
+      console.log('At database.js > mongoose.disconnect()');
+      console.log(error);
+    });
+};
