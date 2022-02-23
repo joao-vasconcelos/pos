9be08324f81c4896e32ef7d522d5a24574b4febf
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-02-23 00:30:14 +0000

    Created _document.js
    As per the ESLint warnings, _document.js was created to include typekit links and favicons.

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name='apple-mobile-web-app-title' content='Register' />
+        <meta name='application-name' content='Register' />
+        <meta name='description' content='CHEF POINT Register Terminal' />
+        <meta name='msapplication-TileColor' content='#ffffff' />
+        <meta name='theme-color' content='#ffffff' />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#ff0000' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+        <meta name='HandheldFriendly' content='true' />
+        <link rel='stylesheet' href='https://use.typekit.net/tto7hgg.css' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
