b14000e72da59456e27005ec0103201c0c7a8919
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-07-02 11:35:40 +0100

    Fix for incorrect path in imports

--- pages/api/customers/[_id]/edit.js
+++ pages/api/customers/[_id]/edit.js
@@ -1,5 +1,5 @@
-import database from '../../../services/database';
-import Customer from '../../../models/Customer';
+import database from '../../../../services/database';
+import Customer from '../../../../models/Customer';
 
 /* * */
 /* EDIT CUSTOMER */
