34f24c4b4dfa3f15624bf003f61136f7bc7a3bae
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-07-02 11:35:20 +0100

    Simplified router.query in URL
    Using [device_code]. Index.js is now error.

---
+++ pages/[device_code].js
@@ -0,0 +1,114 @@
+import { useEffect, useContext, useState } from 'react';
+import { Appstate } from '../context/Appstate';
+import { useRouter } from 'next/router';
+import { styled } from '@stitches/react';
+import useSWR from 'swr';
+import Loading from '../components/Loading';
+import Overlay from '../components/Overlay';
+import FolderGrid from '../modules/folders/FolderGrid';
+import ProductGrid from '../modules/products/ProductGrid';
+import AssociateCustomer from '../modules/customers/AssociateCustomer';
+import OrderDetails from '../modules/order/OrderDetails';
+import OrderTotals from '../modules/order/OrderTotals';
+import UserButton from '../modules/users/UserButton';
+import Discounts from '../modules/discounts/Discounts';
+
+/* * */
+/* POINT OF SALE */
+/* Explanation needed. */
+/* * */
+
+/* */
+/* STYLES */
+
+const Container = styled('div', {
+  position: 'fixed',
+  top: '0',
+  left: '0',
+  display: 'flex',
+  flexDirection: 'row',
+  gap: '$md',
+  alignItems: 'stretch',
+  justifyContent: 'stretch',
+  minHeight: 'var(--window-inner-height)',
+  width: '100%',
+  height: '100%',
+  padding: '$sm',
+  backgroundColor: '$gray0',
+});
+
+const ProductsContainer = styled('div', {
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'stretch',
+  justifyContent: 'stretch',
+});
+
+const CheckoutPannel = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'stretch',
+  justifyContent: 'stretch',
+  minWidth: '350px',
+  borderRadius: '$md',
+  backgroundColor: '$gray0',
+  boxShadow: '$md',
+  overflow: 'scroll',
+});
+
+const InnerCheckoutWrapper = styled('div', {
+  height: '100%',
+  flexGrow: 0,
+  display: 'flex',
+  overflow: 'hidden',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+});
+
+export default function PointOfSale() {
+  //
+
+  const router = useRouter();
+  const { device_code } = router.query;
+
+  const { data: device } = useSWR(device_code ? `/api/devices/${device_code}` : null);
+  const { data: customers } = useSWR('/api/customers/');
+
+  const appstate = useContext(Appstate);
+
+  // Set device data in Appstate
+  useEffect(() => {
+    if (router.isReady) {
+      if (device_code && !device?.isError) appstate.setDevice(device);
+      else router.push('/'); // Navigate to index.js
+    }
+  }, [appstate, device, device_code, router]);
+
+  // Fix window height on component mount
+  useEffect(() => document.documentElement.style.setProperty('--window-inner-height', `${window.innerHeight}px`), []);
+
+  /* */
+  /* RENDER */
+
+  return device && customers ? (
+    <Container>
+      <ProductsContainer>
+        <FolderGrid />
+        <ProductGrid />
+      </ProductsContainer>
+      <CheckoutPannel>
+        <UserButton />
+        <AssociateCustomer />
+        <InnerCheckoutWrapper>
+          <OrderDetails />
+          <Discounts />
+        </InnerCheckoutWrapper>
+        <OrderTotals />
+      </CheckoutPannel>
+      <Overlay />
+    </Container>
+  ) : (
+    <Loading />
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,4 @@
-import { useEffect, useContext, useState } from 'react';
-import { Appstate } from '../context/Appstate';
-import { useRouter } from 'next/router';
 import { styled } from '@stitches/react';
-import useSWR from 'swr';
-import Loading from '../components/Loading';
-import Overlay from '../components/Overlay';
-import FolderGrid from '../modules/folders/FolderGrid';
-import ProductGrid from '../modules/products/ProductGrid';
-import AssociateCustomer from '../modules/customers/AssociateCustomer';
-import OrderDetails from '../modules/order/OrderDetails';
-import OrderTotals from '../modules/order/OrderTotals';
-import UserButton from '../modules/users/UserButton';
-import Discounts from '../modules/discounts/Discounts';
-import ErrorOverlay from '../components/ErrorOverlay';
 
 /* * */
 /* POINT OF SALE */
@@ -23,99 +9,31 @@ import ErrorOverlay from '../components/ErrorOverlay';
 /* STYLES */
 
 const Container = styled('div', {
-  position: 'fixed',
-  top: '0',
-  left: '0',
   display: 'flex',
-  flexDirection: 'row',
-  gap: '$md',
-  alignItems: 'stretch',
-  justifyContent: 'stretch',
-  minHeight: 'var(--window-inner-height)',
-  width: '100%',
-  height: '100%',
-  padding: '$sm',
-  backgroundColor: '$gray0',
-});
-
-const ProductsContainer = styled('div', {
-  width: '100%',
-  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
   flexDirection: 'column',
-  alignItems: 'stretch',
-  justifyContent: 'stretch',
+  gap: '$lg',
+  height: '100vh',
 });
 
-const CheckoutPannel = styled('div', {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'stretch',
-  justifyContent: 'stretch',
-  minWidth: '350px',
-  borderRadius: '$md',
-  backgroundColor: '$gray0',
-  boxShadow: '$md',
-  overflow: 'scroll',
+const Title = styled('p', {
+  fontSize: '30px',
+  fontWeight: '$bold',
+  textTransform: 'uppercase',
 });
 
-const InnerCheckoutWrapper = styled('div', {
-  height: '100%',
-  flexGrow: 0,
-  display: 'flex',
-  overflow: 'hidden',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
+const Subtitle = styled('p', {
+  fontSize: '20px',
+  fontWeight: '$medium',
+  textTransform: 'uppercase',
 });
 
-export default function PointOfSale() {
-  //
-
-  const router = useRouter();
-  const { device_code } = router.query;
-
-  const { data: device } = useSWR('/api/devices/' + (device_code || 'invalid_code'));
-  const { data: customers } = useSWR('/api/customers/');
-
-  const appstate = useContext(Appstate);
-
-  const [isError, setIsError] = useState();
-
-  // Set device data in Appstate
-  useEffect(() => {
-    if (device_code && !device?.isError) {
-      setIsError(false);
-      appstate.setDevice(device);
-    } else setIsError(true);
-  }, [appstate, device, device_code]);
-
-  // Fix window height on component mount
-  useEffect(() => document.documentElement.style.setProperty('--window-inner-height', `${window.innerHeight}px`), []);
-
-  /* */
-  /* RENDER */
-
-  return device && customers ? (
-    isError ? (
-      <ErrorOverlay />
-    ) : (
-      <Container>
-        <ProductsContainer>
-          <FolderGrid />
-          <ProductGrid />
-        </ProductsContainer>
-        <CheckoutPannel>
-          <UserButton />
-          <AssociateCustomer />
-          <InnerCheckoutWrapper>
-            <OrderDetails />
-            <Discounts />
-          </InnerCheckoutWrapper>
-          <OrderTotals />
-        </CheckoutPannel>
-        <Overlay />
-      </Container>
-    )
-  ) : (
-    <Loading />
+export default function IsError() {
+  return (
+    <Container>
+      <Title>Invalid Code</Title>
+      <Subtitle>Shake your device to fix</Subtitle>
+    </Container>
   );
 }
