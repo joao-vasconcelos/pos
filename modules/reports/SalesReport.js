e1bcc75d3cde20c92fbbda53d10bd6f84ab48010
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-07-12 20:12:34 +0100

    Messing with tabs?

--- modules/reports/SalesReport.js
+++ modules/reports/SalesReport.js
@@ -1,5 +1,6 @@
 import { styled } from '@stitches/react';
 import Pannel from '../../components/Pannel';
+import { Tabs } from '@mantine/core';
 
 /* * */
 /* SALES REPORT */
@@ -19,7 +20,11 @@ const Explanation = styled('div', {
 export default function SalesReport() {
   return (
     <Pannel title={'Relatório de Vendas'}>
-      <Explanation>ajhdaisu</Explanation>
+      <Tabs color='dark' variant='pills'>
+        <Tabs.Tab label='Gallery'>Gallery tab content</Tabs.Tab>
+        <Tabs.Tab label='Messages'>Messages tab content</Tabs.Tab>
+        <Tabs.Tab label='Settings'>Settings tab content</Tabs.Tab>
+      </Tabs>
     </Pannel>
   );
 }
