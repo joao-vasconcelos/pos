b30d39b8b785ea8889f45addc54b5826d0764e22
João de Vasconcelos <johnyvasconcelos@icloud.com>
2022-07-19 17:54:07 +0100

    Just removing unnecessary lines

--- pages/api/customers/index.js
+++ pages/api/customers/index.js
@@ -12,8 +12,7 @@ export default async function getAllCustomers(req, res) {
   // 0. Refuse request if not GET
   if (req.method != 'GET') {
     await res.setHeader('Allow', ['GET']);
-    await res.status(405).json({ message: `Method ${req.method} Not Allowed` });
-    return;
+    return await res.status(405).json({ message: `Method ${req.method} Not Allowed` });
   }
 
   // 1. Try to connect to the database
@@ -21,18 +20,15 @@ export default async function getAllCustomers(req, res) {
     await database.connect();
   } catch (err) {
     console.log(err);
-    await res.status(500).json({ message: 'Database connection error.' });
-    return;
+    return await res.status(500).json({ message: 'Database connection error.' });
   }
 
   // 2. Try to fetch all customers from the database
   try {
     const allCustomers = await Customer.find({});
-    await res.status(200).send(allCustomers);
-    return;
+    return await res.status(200).send(allCustomers);
   } catch (err) {
     console.log(err);
-    await res.status(500).json({ message: 'Cannot fetch customers.' });
-    return;
+    return await res.status(500).json({ message: 'Cannot fetch customers.' });
   }
 }
